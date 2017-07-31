e4fbb0ccf5e1d4194b7bcfea4f77a5992ecaf6db
ramon-wsg <rdejonge@wellservices-group.com>
2017-07-31 09:31:42 +0200

    Initial commit

---
+++ src/Scopes/DataTableScope.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace WellServicesGroup\DataTableScopes\Scopes;
+
+use Yajra\Datatables\Contracts\DataTableScopeContract;
+
+class DataTableScope implements DataTableScopeContract
+{
+    protected $functionName;
+    protected $arguments;
+
+    public function __construct(string $functionName, array $arguments)
+    {
+        $this->functionName = $functionName;
+        $this->arguments = $arguments;
+    }
+
+    /**
+     * Apply a query scope.
+     *
+     * @param $query
+     * @return mixed
+     */
+    public function apply($query)
+    {
+        $functionName = $this->functionName;
+
+        return $query->$functionName(...$this->arguments);
+    }
+}
